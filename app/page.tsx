030fc5b08c8b419e22be4d105f15de5cd0c79fc6
Jarosław Chudzik <jaroslaw.chudzik@agora.pl>
2024-08-13 12:12:08 +0200

    imporve code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 // Istnieje prawdopodobieństwo 10^(-size) że uuid się powtórzy
-export const generateID = (size: number = 9) => {
+const generateID = (size: number = 9) => {
   return String(Math.random()).slice(-size);
 };
 
@@ -11,19 +11,40 @@ const Test = () => {
   const [log, setLog] = useState<string[]>([]);
 
   const handleEvent =
-    (name: string) => (e: React.KeyboardEvent<HTMLInputElement>) => {
-      setLog((prev) => [...prev, `
+    (name: string) =>
+    (
+      e:
+        | React.KeyboardEvent<HTMLInputElement>
+        | React.ClipboardEvent<HTMLInputElement>
+    ) => {
+      if ("key" in e) {
+        setLog((prev) => [
+          ...prev,
+          `
         <strong>${name}</strong><br/>
         ${e.type} ${e.key} ${e.code} ${e.keyCode} ${e.charCode} ${e.which}
-      `]);
+      `,
+        ]);
+        return;
+      }
+
+      const clipboardData = e.clipboardData?.getData("text");
+
+      setLog((prev) => [
+        ...prev,
+        `
+        <strong>${name}</strong><br/>
+        ${e.type} ${clipboardData} ${e.clipboardData?.types}
+      `,
+      ]);
     };
   return (
     <>
-    <div className="flex gap-2">
-      <input
-        type="number"
-        maxLength={1}
-        className={`
+      <div className="flex gap-2">
+        <input
+          type="number"
+          maxLength={1}
+          className={`
           h-10 w-[20rem] max-w-full border-b-2
           text-center font-montserrat
           text-2xl font-semibold text-black outline-none
@@ -32,19 +53,24 @@ const Test = () => {
           [&::-webkit-outer-spin-button]:appearance-none
           rounded
         `}
-        pattern="\d"
-        min="0"
-        inputMode="numeric"
-        onKeyPress={handleEvent("onKeyPress")}
-        onKeyUp={handleEvent("onKeyUp")}
-        onKeyDown={handleEvent("onKeyDown")}
-        onPaste={handleEvent("onPaste")}
-      />
-        <button className="border b-white rounded p-2 text-xs" onClick={() => setLog([])}>Clear</button>
-        </div>
+          pattern="\d"
+          min="0"
+          inputMode="numeric"
+          onKeyPress={handleEvent("onKeyPress")}
+          onKeyUp={handleEvent("onKeyUp")}
+          onKeyDown={handleEvent("onKeyDown")}
+          onPaste={handleEvent("onPaste")}
+        />
+        <button
+          className="border b-white rounded p-2 text-xs"
+          onClick={() => setLog([])}
+        >
+          Clear
+        </button>
+      </div>
       <div className="mt-4 flex flex-col gap-2 text-xs">
         {log.toReversed().map((l, i) => (
-          <div key={generateID()} dangerouslySetInnerHTML={{__html: l}} />
+          <div key={generateID()} dangerouslySetInnerHTML={{ __html: l }} />
         ))}
       </div>
     </>
